8a65049cfaab730d86bf33578db77b61c882d776
cardoso-thiago <thiago.cardoso33@gmail.com>
2022-06-21 00:07:55 -0300

    Commit inicial

---
+++ src/main/java/br/com/cardoso/DemoTrivyApplication.java
@@ -0,0 +1,13 @@
+package br.com.cardoso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoTrivyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoTrivyApplication.class, args);
+	}
+
+}
